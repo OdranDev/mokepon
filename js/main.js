98778c8ceab20160a941650bb7b3fb2640a6be62
odran21 <jleonardoguevaram@gmail.com>
2023-07-03 07:57:03 -0500

    Boton seleccionar mascota

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function iniciarJuego() {
+    
+}
+
+function seleccionarMascota() {
+    alert('Seleccionaste tu mascota');
+}
+
+let btnMascota = document.getElementById('btn-Mascota');
+btnMascota.addEventListener('click', seleccionarMascota);
+
+window.addEventListener('load, iniciarJuego')

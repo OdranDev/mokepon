ed2d55fcc16be6c8988e78935b69b95437327f6c
odran21 <jleonardoguevaram@gmail.com>
2023-07-03 19:22:56 -0500

    Seleccionando Mascota

--- js/main.js
+++ js/main.js
@@ -1,12 +1,26 @@
 function iniciarJuego() {
-    
+    let btnMascota = document.getElementById('btn-Mascota')
+btnMascota.addEventListener('click', seleccionarMascota)
 }
 
 function seleccionarMascota() {
-    alert('Seleccionaste tu mascota');
+    let inputHipodoge = document.getElementById('Hipodoge')
+    let inputCapipepo = document.getElementById('Capipepo')
+    let inputRatigüeya = document.getElementById('Ratigüeya')
+    let spanNombreMascota = document.getElementById('nombreMascota')
+    if (inputHipodoge.checked) {
+        spanNombreMascota.innerHTML = 'Hipodoge'
+        alert('Seleccionaste a Hipodoge');
+    } else if (inputCapipepo.checked) {
+        spanNombreMascota.innerHTML = 'Capipepo'
+        alert('Seleccionaste a Capipepo');
+    } else if (inputRatigüeya.checked) {
+        spanNombreMascota.innerHTML = 'Ratigüeya'
+        alert('Seleccionaste a Ratigüeya');
+    } else {
+        alert('Debes seleccionar una mascota')
+    }
+    
 }
 
-let btnMascota = document.getElementById('btn-Mascota');
-btnMascota.addEventListener('click', seleccionarMascota);
-
-window.addEventListener('load, iniciarJuego')
+window.addEventListener('load', iniciarJuego)
